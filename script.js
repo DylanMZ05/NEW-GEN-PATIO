aecbdd4ffe05c23c3d4c8dcf2a609ed5f209ecbf
DylanMZ <dylanmazurzgainer@gmail.com>
2024-10-28 22:48:48 -0300

    a

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active'); // Activa o desactiva la clase `active` en el menú
     hamburger.classList.toggle('open'); // Cambia el ícono de hamburguesa a una "X"
 });
+
+
+
+const scrollingText = document.getElementById('scrollingText');
+
+function stopAnimation() {
+    scrollingText.style.animationPlayState = 'paused';
+}
+
+function resumeAnimation() {
+    scrollingText.style.animationPlayState = 'running';
+}
+
+// Para escritorio: detener y reanudar con el mouse
+scrollingText.addEventListener('mouseenter', stopAnimation);
+scrollingText.addEventListener('mouseleave', resumeAnimation);
+
+// Para dispositivos móviles: detener y reanudar al tocar
+scrollingText.addEventListener('touchstart', stopAnimation);
+scrollingText.addEventListener('touchend', resumeAnimation);
