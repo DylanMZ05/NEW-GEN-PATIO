4c6fb7de3a37621029bbda16bb2b2674c9a2ab5a
DylanMZ <dylanmazurzgainer@gmail.com>
2024-11-19 20:17:11 -0300

    CHANGES

--- js/script.js
+++ js/script.js
@@ -32,13 +32,13 @@ function resumeAnimation() {
     scrollingText.style.animationPlayState = 'running';
 }
 
-// // Para escritorio: detener y reanudar con el mouse
-// scrollingText.addEventListener('mouseenter', stopAnimation);
-// scrollingText.addEventListener('mouseleave', resumeAnimation);
+// // // Para escritorio: detener y reanudar con el mouse
+// // scrollingText.addEventListener('mouseenter', stopAnimation);
+// // scrollingText.addEventListener('mouseleave', resumeAnimation);
 
-// // Para dispositivos móviles: detener y reanudar al tocar
-// scrollingText.addEventListener('touchstart', stopAnimation);
-// scrollingText.addEventListener('touchend', resumeAnimation);
+// // // Para dispositivos móviles: detener y reanudar al tocar
+// // scrollingText.addEventListener('touchstart', stopAnimation);
+// // scrollingText.addEventListener('touchend', resumeAnimation);
 
 
 
@@ -70,156 +70,17 @@ function toggleFaq(element) {
 
 /* FREE QUOTE */
 
-//References
-// const a = document.getElementById('a'); // Primer paso
-// const pergola = document.getElementById('pergola');
-// const subOptionContainer = document.getElementById('sub-option-container'); // Segundo paso
-// const subOptionContainer2 = document.getElementById('sub-option-container2'); // Segundo paso 2
-// const stepContainer = document.getElementById('step-container'); // Tercer paso
-// const lastStep = document.getElementById('last-step'); // Último paso
-// const btns = document.getElementById('continue-btn'); // Botón de continuar
-// const backBtn = document.getElementById('back-btn'); // Botón de regresar
-// const titleTemplate = document.getElementById('title-template');
-// const textTemplate = document.getElementById('text-template-1');
-// const textTemplate2 = document.getElementById('text-template-2');
-// const img1Template = document.getElementById('img-template-1');
-// const img2Template = document.getElementById('img-template-2');
-
-// let currentStep = 1;
-
-// pergola.addEventListener('click', showSubOptions)
-// backBtn.addEventListener('click', goBack);
-
-// function showSubOptions() {
-//     a.style.display = 'none';
-//     subOptionContainer.style.display = 'flex';
-//     btns.style.display = 'flex';
-//     currentStep = 2;
-// }
-
-// function showSubOptions() {
-//     a.style.display = 'none';
-//     subOptionContainer.style.display = 'flex';
-//     btns.style.display = 'flex';
-//     currentStep = 2;
-// }
-
-// function goNextStep() {
-//     if (currentStep === 2) {
-//         subOptionContainer.style.display = 'none';
-//         stepContainer.style.display = 'flex';
-//         btns.style.display = 'none';
-//         currentStep = 3;
-//     }
-// }
-
-// function goBack() {
-//     if (currentStep === 2) {
-//         subOptionContainer.style.display = 'none';
-//         a.style.display = 'flex';
-//         btns.style.display = 'none';
-//         currentStep = 1;
-//     }
-//     else if (currentStep === 3) {
-//         subOptionContainer.style.display = 'flex';
-//         btns.style.display = 'flex';
-//         currentStep = 2;
-//     }
-
-//     console.log(goBack)
-// }
-
-// // RAMA 1
-
-// document.querySelector('.next-btn').addEventListener('click', goNextStep);
-// img1Template.addEventListener('click', () => changeSteps());
-
-// const steps = {
-//     1: {
-//         title: 'FOUNDATION',
-//         img: 'media/Bradon-Rose/Newgenpatio_1-4.jpg',
-//         text: 'GLASS',
-//         img2: 'media/Bradon-Rose/Newgenpatio_1-5.jpg',
-//         text2: 'CONCRETE',
-//     }
-
-// };
-// let numberStep = 0;
-
-// function changeSteps(){
-//     numberStep += 1;
-//     const beforeStep = steps[numberStep];
-//     titleTemplate.innerHTML = beforeStep.title;
-//     img1Template.src = beforeStep.img;
-//     textTemplate.innerHTML = beforeStep.text;
-//     img2Template.src = beforeStep.img2;
-//     textTemplate2.innerHTML = beforeStep.text2;
-
-//     if (numberStep === 1) {
-//         document.getElementById('img-template-1').onclick = showNextContainer;
-//         document.getElementById('text-template-1').onclick = showNextContainer;
-//         document.getElementById('img-template-2').onclick = showNextContainer;
-//         document.getElementById('text-template-2').onclick = showNextContainer;
-//     }
-// };
-
-// // RAMA 2
-
-// img2Template.addEventListener('click', () => changeSteps2());
-
-// const steps2 = {
-//     1: {
-//         title: 'FREESTANDING',
-//         img: 'media/Bradon-Rose/Newgenpatio_1-4.jpg',
-//         text: 'RESOLAR',
-//         img2: 'media/Bradon-Rose/Newgenpatio_1-5.jpg',
-//         text2: 'CANTILEVER',
-//     },
-//     2: {
-//         title: 'FUNDATION',
-//         img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg',
-//         text: 'GLASS',
-//         img2: 'media/Bradon-Rose/Newgenpatio_1-3.jpg',
-//         text2: 'CONCRETE',
-//     }
-
-// };
-// let numberStep2 = 0;
-
-// function changeSteps2(){
-//     numberStep += 1;
-//     const beforeStep2 = steps2[numberStep];
-//     titleTemplate.innerHTML = beforeStep2.title;
-//     img1Template.src = beforeStep2.img;
-//     textTemplate.innerHTML = beforeStep2.text;
-//     img2Template.src = beforeStep2.img2;
-//     textTemplate2.innerHTML = beforeStep2.text2;
-
-//     if (numberStep === 2) {
-//         document.getElementById('img-template-1').onclick = showNextContainer;
-//         document.getElementById('text-template-1').onclick = showNextContainer;
-//         document.getElementById('img-template-2').onclick = showNextContainer;
-//         document.getElementById('text-template-2').onclick = showNextContainer;
-//     }
-// };
-
-// function showNextContainer() {
-//     document.getElementById('step-container').style.display = 'none';
-//     document.getElementById('last-step').style.display = 'flex';
-// }
-
-// Definir los pasos con sus detalles
 const steps = {
     1: {
-        title: '¿Qué estás buscando?',
+        title: '¿What are you looking for?',
         progress: 0,
         options: [
-            { text: 'Pergola/Patio', nextStep: 2 },
-            { text: 'Outdoor Kitchen', nextStep: 10 }
-        ]
+            { img: 'media/Pictures-WEBs/Fotos-para-Fondos/Cover Patios & Pergolas.jpg', text: 'Patios & Pergolas', nextStep: 2 },
+            { img: 'media/Pictures-WEBs/Fotos-para-Fondos/Outdoor Kitchens.jpg', text: 'Outdoor Kitchens', nextStep: 2 }
+        ],
     },
     2: {
-        title: 'Ingrese las dimensiones',
+        title: 'Measurement',
         fields: [
             { label: 'Width', id: 'width' },
             { label: 'Length', id: 'length' },
@@ -229,31 +90,32 @@ const steps = {
         previousStep: 1
     },
     3: {
-        title: '¿Cómo prefieres tu proyecto?',
+        title: 'Type',
         options: [
-            { img: 'media/Bradon-Rose/Newgenpatio_1-4.jpg', text: 'ATTACHED', nextStep: 5 },
-            { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'FREESTANDING', nextStep: 4 }
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Attached.jpeg', text: 'Attached', nextStep: 5 },
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Freestanding.jpeg', text: 'Freestanding', nextStep: 4 }
         ],
         previousStep: 2
     },
     4: {
-        title: 'Tipo de Fundación',
+        title: 'Style',
         options: [
-            { img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg', text: 'REGULAR', nextStep: 5 },
-            { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'CANTILEVER', nextStep: 5 }
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Fresstanding/Regular.jpeg', text: 'Regular', nextStep: 5 },
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Fresstanding/Cantilever.jpg', text: 'Cantilever', nextStep: 5 }
         ],
         previousStep: 3
     },
     5: {
-        title: 'Tipo de Material para la Fundación',
+        title: 'Foundation',
         options: [
-            { img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg', text: 'GLASS', nextStep: 'final' },
-            { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'CONCRETE', nextStep: 'final' }
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Foundation/Concrete.jpg', text: 'Concrete', nextStep: 'final' },
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Foundation/Grass & Dirt.jpg', text: 'Grass & Dirt', nextStep: 'final' },
+            { img: 'media/Pictures-WEBs/Free-QUOTE/Patios-&-Pergolas/Foundation/Travertine.jpg', text: 'Travertine', nextStep: 'final' }
         ],
         previousStep: 4
     },
     10: {
-        title: 'Estilo',
+        title: 'Style',
         options: [
             { img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg', text: 'MODERN', nextStep: 11 },
             { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'TRADITIONAL', nextStep: 12 }
@@ -261,7 +123,7 @@ const steps = {
         previousStep: 1
     },
     11: {
-        title: 'Estilo',
+        title: 'Measurement',
         fields: [
             { label: 'Linear Feet', id: 'linear-feet' },
         ],
@@ -269,7 +131,7 @@ const steps = {
         previousStep: 10
     },
     12: {
-        title: 'Estilo',
+        title: 'Measurement',
         fields: [
             { label: 'Linear Feet', id: 'linear-feet' },
         ],
@@ -277,7 +139,7 @@ const steps = {
         previousStep: 10
     },
     13: {
-        title: 'Estilo',
+        title: 'Exterior Material',
         options: [
             { img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg', text: 'COMPOSITE', nextStep: 'final' },
             { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'THERMO WOOD', nextStep: 'final' }
@@ -286,7 +148,7 @@ const steps = {
         previousStep: 11
     },
     14: {
-        title: 'Estilo',
+        title: 'Exterior Material',
         options: [
             { img: 'media/Bradon-Rose/Newgenpatio_1-3.jpg', text: 'COMPOSITE', nextStep: 'final' },
             { img: 'media/Bradon-Rose/Newgenpatio_1-5.jpg', text: 'THERMO WOOD', nextStep: 'final' },
@@ -317,7 +179,7 @@ function renderStep(stepNumber) {
         const optionsContainer = document.createElement('div');
         optionsContainer.classList.add('image-options-container');
 
-        if (stepNumber === 14) {
+        if (stepNumber === 14 || stepNumber === 5) {
             optionsContainer.classList.add('step-14');
         }
 
@@ -378,7 +240,7 @@ function renderStep(stepNumber) {
     if (step.previousStep) {
         const backButton = document.createElement('button');
         backButton.classList.add('back-btn');
-        backButton.textContent = 'Regresar';
+        backButton.textContent = 'Back';
         backButton.onclick = () => goToPreviousStep(step.previousStep);
         buttonContainer.appendChild(backButton);
     }
@@ -387,7 +249,7 @@ function renderStep(stepNumber) {
     if (step.fields && step.nextStep) {
         const nextButton = document.createElement('button');
         nextButton.classList.add('next-btn');
-        nextButton.textContent = 'Siguiente';
+        nextButton.textContent = 'Continue';
         nextButton.onclick = () => goToNextStep(step.nextStep);
         buttonContainer.appendChild(nextButton);
     }
@@ -447,7 +309,7 @@ function showFinalStep() {
     const backButton = document.createElement('button');
     backButton.classList.add('back-btn');
     backButton.textContent = 'Regresar';
-    backButton.onclick = () => goToPreviousStep(currentStep - 1);
+    backButton.onclick = () => goToPreviousStep(currentStep);
     buttonContainer.appendChild(backButton);
 
     container.appendChild(buttonContainer);
