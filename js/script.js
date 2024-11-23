c78e5ca5de61b9ab2a5266c8e91608d63eb35c6c
DylanMZ <dylanmazurzgainer@gmail.com>
2024-11-23 14:53:42 -0300

    FOOTER CUESTIONARIE

--- js/script.js
+++ js/script.js
@@ -68,3 +68,23 @@ function toggleFaq(element) {
 
 
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_01v4ztg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
