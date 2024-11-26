c2dec2221321c149b764d6aec9adbe80646f20c4
DylanMZ <dylanmazurzgainer@gmail.com>
2024-11-26 19:44:13 -0300

    ELIMINACIÓN BOT

--- js/script.js
+++ js/script.js
@@ -40,6 +40,18 @@ scrollingText.addEventListener('mouseleave', resumeAnimation);
 scrollingText.addEventListener('touchstart', stopAnimation);
 scrollingText.addEventListener('touchend', resumeAnimation);
 
+function openModal(img) {
+    const modal = document.getElementById('imgModal');
+    const modalImg = document.getElementById('imgModalContent');
+    modal.style.display = 'flex';
+    modalImg.src = img.src; // Establece la fuente de la imagen al modal
+}
+
+// Cierra el modal
+function closeModal() {
+    const modal = document.getElementById('imgModal');
+    modal.style.display = 'none';
+}
 
 
 function toggleFaq(element) {
